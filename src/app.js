7bcb1e8470eec00debb133bc350b560336785168
qunaxis <qunaxis@gmail.com>
2019-04-27 14:03:21 +0300

    Intial

---
+++ src/app.js
@@ -0,0 +1,53 @@
+import express from 'express'
+import path from 'path'
+import logger from 'morgan'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+import routes from './routes'
+
+const app = express()
+
+app.use(cors())
+app.disable('x-powered-by')
+
+// View engine setup
+app.set('views', path.join(__dirname, '../views'))
+app.set('view engine', 'pug')
+
+app.use(logger('dev', {
+  skip: () => app.get('env') === 'test'
+}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, '../public')))
+
+// Routes
+// app.use('/', routes)
+
+// SEND SPA TO CLIENT + CLIENT ROUTING :)
+app.get('*', (req, res, next) => {
+  res.sendFile('index.html', { root: path.join(__dirname, '../public') })
+})
+app.post('/gethuy', (req, res, next) => {
+  // res.sendFile('index.html', { root: path.join(__dirname, '../public') })
+  res.json({ get: "huy" })
+})
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// Error handler
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  res
+    .status(err.status || 500)
+    .render('error', {
+      message: err.message
+    })
+})
+
+export default app
