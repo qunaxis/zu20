5aaefe7d27ff2794bca6a0b8c6ec9aececd997c4
qunaxis <qunaxis@gmail.com>
2020-01-24 16:56:13 +0300

    LOGS ENABLED URA

--- src/bot.js
+++ src/bot.js
@@ -19,6 +19,66 @@ let bot = {}
 let chatId = 293233794
 
 
+
+const getStatus = async () => {
+    const settingsArray = await db.Setting.findAll()
+    let settings = {}
+    if(settingsArray) { 
+        for (const setting of settingsArray) {
+            settings[setting.dataValues.parameter] = setting.dataValues.value
+        }
+    } else { 
+        console.log(new Error(settings))
+    }
+    console.log(settings)
+    return settings
+}
+
+const setWarn = async (newWarn) => {
+    console.log(`SET WARN: ${newWarn.hash} ${newWarn.value} ${newWarn.reason}`)
+    console.log(newWarn)
+    const options = {
+        fields: ['hash', 'value', 'reason', 'author'] 
+    }
+
+    const immun = await db.Immun.findOne({ where: {
+        hash: newWarn.hash
+    }})
+    console.log(immun)
+    const iHash = immun.dataValues.hash
+    const warn = await db.Warn.create({
+        hash: iHash,
+        value: newWarn.value,
+        reason: newWarn.reason,
+        author: newWarn.author
+    }, options)
+    const warnData = {
+        hash: immun.dataValues.hash,
+        secondname: immun.dataValues.secondname,
+        firstname: immun.dataValues.firstname,
+        faculty: immun.dataValues.faculty,
+        value: warn.dataValues.value,
+        reason: warn.dataValues.reason
+    }
+    return warnData
+}
+
+// Нужно переопределить на уровне приложения!
+const setParameter = async (newParameter, newValue) => {
+    console.log(`SETTING PARAMETERS: ${newParameter}: ${newValue}`)
+    const updatedSetting = await db.Setting.update({
+            parameter: newParameter,
+            value: newValue
+        }, {
+            where: {
+                parameter: newParameter
+            }
+        })
+    console.log(updatedSetting[0] == 1)
+    return updatedSetting[0] == 1 ? true : false
+}
+
+
 try {
     bot = new Telegraf(BOT_TOKEN, { agent: socksAgent })
     bot.telegram.sendMessage(chatId, 'Я перезагрузился и теперь активен. Жду Ваших приказаний, сэр!')
@@ -32,7 +92,7 @@ bot.command(`/infected`, async (ctx) => {
     console.log(ctx.message)
     let message = ctx.message.text.split(' ')
     if(message[1] != undefined) {
-        let updValue = await db.setParameter('infected', message[1])
+        let updValue = await setParameter('infected', message[1])
         ctx.reply(`Обновлено. Инфицированных: ${updValue}%`)
     } else {
         ctx.reply('Для обновления: /infected 54')
@@ -42,7 +102,7 @@ bot.command(`/antidot`, async (ctx) => {
     console.log(ctx.message)
     let message = ctx.message.text.split(' ')
     if(message[1] != undefined) {
-        let updValue = await db.setParameter('infected', message[1])
+        let updValue = await setParameter('infected', message[1])
         ctx.reply(`Обновлено. Готовность антидота: ${updValue}%`)
     } else {
         ctx.reply('Для обновления: /antidot 54')
@@ -52,7 +112,7 @@ bot.command(`/timer`, async (ctx) => {
     console.log(ctx.message)
     let message = ctx.message.text.split(' ')
     if(message[1] != undefined) {
-        let updValue = await db.setParameter('timer', message[1])
+        let updValue = await setParameter('timer', message[1])
         updValue ? ctx.reply(`Обновлено. Таймер до: ${updValue}`) : ctx.reply(`Что-то пошло не так :()`)
     } else {
         ctx.reply('Для обновления: /timer 23:00')
@@ -71,13 +131,13 @@ bot.command(`/warn`, async ctx => {
     message.shift()
     warn.reason = message.join(' ')
     // Внесение предупреждения в базу
-    const warnData = await db.setWarn(warn)
+    const warnData = await setWarn(warn)
     console.log(warnData)
     ctx.reply(`Иммун ${warnData.hash} ${warnData.firstname} ${warnData.secondname} (${warnData.faculty}) получил снижение иммунитета на ${warn.value}% по причине: ${warn.reason}`)
 })
 bot.command(`/status`, async (ctx) => {
-    const status = await db.getStatus()
-    console.log(status)
+    // const status = await getStatus()
+    // console.log(status)
     const { antidot, infected, deadline } = await getStatus()
     ctx.reply(`Прогресс разработки антидота: ${antidot}%\nДоля зараженных: ${infected}%\nВремя таймера: ${deadline}`)
 })
