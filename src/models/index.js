4c501d86829bd4b383f271a1a39654da761506e2
qunaxis <qunaxis@gmail.com>
2020-01-26 01:56:12 +0300

    Fix qrs

--- src/models/index.js
+++ src/models/index.js
@@ -128,15 +128,15 @@ const genQrs = async (data) => {
         // }, () => console.log('OK'))  
         // await fs.mkdir(qrPath)
         qr.save(path.join(__dirname, `../../docs/qrs/${item.id + ' ' + item.secondname + ' ' + item.firstname}.svg`))
-        let output = fs.createWriteStream(zipPath)
-        let archive = archiver('zip', {
-            zlib: { level: 9 } // Sets the compression level.
-        })
-        archive.pipe(output)
-        archive.directory(path.join(__dirname, `../../docs/qrs`), false)
-        archive.finalize()
-        urlData.push(item)
     }
+    let output = fs.createWriteStream(zipPath)
+    let archive = archiver('zip', {
+        zlib: { level: 9 } // Sets the compression level.
+    })
+    archive.pipe(output)
+    archive.directory(path.join(__dirname, `../../docs/qrs`), false)
+    archive.finalize()
+    urlData.push(item)
     return urlData
 }
 
